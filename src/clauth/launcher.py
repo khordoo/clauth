6f344c1ab0e6b1733cc9614aad26d97e0fd3778f
Mahmood Khordoo <m.khordoo@gmail.com>
2025-09-26 14:55:46 -0400

    Add copyright headers

--- src/clauth/launcher.py
+++ src/clauth/launcher.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 Mahmood Khordoo
+#
+# This software is licensed under the MIT License.
+# See the LICENSE file in the root directory for details.
+
 """
 CLAUTH Application Launcher.
 
